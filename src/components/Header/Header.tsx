b2f32e3b3031e08d452292d878a6b515aa2aefed
Rosa <costurezati@gmail.com>
2026-06-02 13:33:52 -0300

    .

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,23 @@
 import styles from './style.module.css';
+import logo from '../assets/IMG/logo.svg';
 
 export const Header: React.FC = () =>{
   return(
-    <header>
-      
+        <header className={styles.headerbg}>
+    <div className={styles.container}>
+      <a href="./index.html">
+        <picture className={styles.logo}><img src={logo} alt=""></picture>
+      </a>
+      <nav className="nav-links">
+        <ul className="header-menu font-1-m">
+          <li><a href="./minhasreceitas.html">Minhas Receitas</a></li>
+          <li><a href="./addreceita.html">Adicionar Receitas</a></li>
+          <li><a href="./sobre.html">sobre nós</a></li>
+          <li><a href="./login.html"><i className="fa-regular fa-circle-user"></i></a></li>
+        </ul>
+      </nav>
+    </div>
     </header>
-  )
+
+  );
 }
